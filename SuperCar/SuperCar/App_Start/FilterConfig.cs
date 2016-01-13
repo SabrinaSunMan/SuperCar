9d7dec251bb83b19024af4e8e4b7321af6cf12bd
SabrinaSunMan <jane199141@gmail.com>
2016-01-13 10:47:08 +0800

    SuperCarMVC

---
+++ SuperCar/SuperCar/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SuperCar
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
